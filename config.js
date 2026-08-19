7ea8ce09ffcffda8b49559766c0fbca75337fc4d
David-Lucas17 <lucasehermilda@gmail.com>
2026-08-18 22:58:54 -0300

    feat: add Supabase configuration

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://zmymvkmjrpryjxiqmxpk.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_uASZk4wNsVg-WSar78G5Mw_JMigOIIX';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
